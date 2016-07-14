a52ccfa2ff38908833f1df7b163b6004c265d8d9
Maik Kempe <mkempe@bitaculous.com>
2016-07-14 20:34:30 +0200

    [➦] Merged changes.

--- Configuration/TypoScript/Vendor/setup.ts
+++ Configuration/TypoScript/Vendor/setup.ts
@@ -24,3 +24,24 @@ lib {
 # === Grid Elements (gridelements) ===
 
 <INCLUDE_TYPOSCRIPT: source="FILE: EXT:gridelements/Configuration/TypoScript/setup.ts">
+
+# tt_content {
+#   gridelements_pi1 >
+#
+#   gridelements_pi1 = COA
+#   gridelements_pi1 {
+#     # 10 =< lib.stdheader
+#
+#     20 = COA
+#     20 {
+#       10 = USER
+#       10 {
+#         userFunc = GridElementsTeam\Gridelements\Plugin\Gridelements->main
+#
+#         setup {
+#           default < lib.gridelements.defaultGridSetup
+#         }
+#       }
+#     }
+#   }
+# }
