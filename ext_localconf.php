b2711d0fdcaff78fe6bd48228aa10d25533c46d2
Maik Kempe <mkempe@bitaculous.com>
2017-01-31 14:12:11 +0100

    [➠] Use `call_user_func` in `ext_localconf.php` and `ext_tables.php`.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,8 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-// === TsConfig ===
+call_user_func(function ($extkey) {
+  // === TsConfig ===
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3v_content/Configuration/TSconfig.ts">');
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extkey . '/Configuration/TSconfig.ts">');
+}, $_EXTKEY);
