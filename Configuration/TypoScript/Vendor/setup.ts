682402f8d3b88c5a86ef339c752c7644eae5c3a3
Maik Kempe <mkempe@bitaculous.com>
2017-07-27 16:53:11 +0200

    [➠] Updated Compat Versions.

--- Configuration/TypoScript/Vendor/setup.ts
+++ Configuration/TypoScript/Vendor/setup.ts
@@ -1,10 +1,10 @@
 # === Fluid Styled Content (fluid_styled_content) ===
 
-[compatVersion = 7.6.0]
+[compatVersion = 7.6]
 <INCLUDE_TYPOSCRIPT: source="FILE: EXT:fluid_styled_content/Configuration/TypoScript/Static/setup.txt">
 [global]
 
-[compatVersion = 8.7.0]
+[compatVersion = 8.7]
 <INCLUDE_TYPOSCRIPT: source="FILE: EXT:fluid_styled_content/Configuration/TypoScript/setup.txt">
 [global]
 
