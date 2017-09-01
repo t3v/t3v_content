fde158162df7fffa08928415a6e136e90209ad21
Maik Kempe <mkempe@bitaculous.com>
2017-09-01 02:17:24 +0200

    [➠] Formatted code [ci skip].

--- Configuration/TSconfig/GridElements.ts
+++ Configuration/TSconfig/GridElements.ts
@@ -1,7 +1,5 @@
 # === Includes ===
 
 <INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/ColumnLayoutGridElement.ts">
-
 <INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/ViewportGridElement.ts">
-
 <INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/AccordionGridElement.ts">
