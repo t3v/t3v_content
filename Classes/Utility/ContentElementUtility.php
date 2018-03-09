b3fe141851ecd64cb71c1d44ee483894a441aee6
Maik Kempe <mkempe@bitaculous.com>
2018-03-10 00:05:48 +0100

    [➠] Implemented `ContentElementUtility`.

--- Classes/Utility/ContentElementUtility.php
+++ Classes/Utility/ContentElementUtility.php
@@ -1,12 +1,49 @@
 <?php
 namespace T3v\T3vContent\Utility;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
 /**
  * The content element utility class.
  *
  * @package T3v\T3vContent\Utility
  */
 class ContentElementUtility {
+  /**
+   * Gets the content element identifier from a content element key.
+   *
+   * @param string $contentElementKey The content element key
+   * @return string The content element identifier
+   */
+  public static function contentElementIdentifier($contentElementKey) {
+    $contentElementIdentifier = (string) $contentElementKey;
+
+    if (strpos($contentElementIdentifier, '_') ||
+        strpos($contentElementIdentifier, '-') ||
+        strpos($contentElementIdentifier, ' ')) {
+      $contentElementIdentifier    = mb_strtolower($contentElementKey);
+      $contentElementIdentifier    = str_replace('_', ' ', $contentElementIdentifier);
+      $contentElementIdentifier    = str_replace('-', ' ', $contentElementIdentifier);
+      $contentElementIdentifier    = str_replace(' ', '', ucwords($contentElementIdentifier));
+    }
+
+    if ($contentElementIdentifier[0]) {
+      $contentElementIdentifier[0] = mb_strtoUpper($contentElementIdentifier[0]);
+    }
+
+    return $contentElementIdentifier;
+  }
+
+  /**
+   * Gets the content element signature from a extension and content element identifier.
+   *
+   * @param string $extensionIdentifier The extension identifier
+   * @param string $contentElementIdentifier The content element identifier
+   * @return string The content element signature
+   */
+  public static function contentElementSignature($extensionIdentifier, $contentElementIdentifier) {
+    $extensionIdentifier      = (string) $extensionIdentifier;
+    $contentElementIdentifier = (string) $contentElementIdentifier;
+    $contentElementSignature  = mb_strtolower($extensionIdentifier . '_' . $contentElementIdentifier);
+
+    return $contentElementSignature;
+  }
 }
--- Tests/Unit/Utility/ContentElementUtilityTest.php
+++ Tests/Unit/Utility/ContentElementUtilityTest.php
@@ -11,4 +11,67 @@ use T3v\T3vContent\Utility\ContentElementUtility;
  * @package T3v\T3vContent\Tests\Unit\Utility
  */
 class ContentElementUtilityTest extends UnitTestCase {
+  /**
+   * Test the content element identifier function.
+   *
+   * @test
+   */
+  public function contentElementIdentifier() {
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('Announcements'),
+      'Announcements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('Announcements Latest Announcements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('announcements latest announcements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('announcements Latest Announcements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('announcements_latest_announcements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('announcements-latest-announcements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('AnnouncementsLatestAnnouncements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementIdentifier('announcementsLatestAnnouncements'),
+      'AnnouncementsLatestAnnouncements'
+    );
+  }
+
+  /**
+   * Test the content element signature function.
+   *
+   * @test
+   */
+  public function contentElementSignature() {
+    $this->assertEquals(
+      ContentElementUtility::contentElementSignature('t3vcore', 'Announcements'),
+      't3vcore_announcements'
+    );
+
+    $this->assertEquals(
+      ContentElementUtility::contentElementSignature('t3vcore', 'AnnouncementsLatestAnnouncements'),
+      't3vcore_announcementslatestannouncements'
+    );
+  }
 }
