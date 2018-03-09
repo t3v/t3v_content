7168e534d29d45bd3bcc70c783b59f2c5c2b3be3
Maik Kempe <mkempe@bitaculous.com>
2018-03-09 21:44:24 +0100

    [➠] Added `ContentElementUtility`.

---
+++ Classes/Utility/ContentElementUtility.php
@@ -0,0 +1,12 @@
+<?php
+namespace T3v\T3vContent\Utility;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * The content element utility class.
+ *
+ * @package T3v\T3vContent\Utility
+ */
+class ContentElementUtility {
+}
---
+++ Tests/Unit/Utility/ContentElementUtilityTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace T3v\T3vContent\Tests\Unit\Utility;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+use T3v\T3vContent\Utility\ContentElementUtility;
+
+/**
+ * The content element utility test class.
+ *
+ * @package T3v\T3vContent\Tests\Unit\Utility
+ */
+class ContentElementUtilityTest extends UnitTestCase {
+}
