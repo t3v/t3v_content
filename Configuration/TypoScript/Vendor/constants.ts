3dc050ae4329d537b8b7810e57b6a7ab45f870cc
Maik Kempe <mkempe@bitaculous.com>
2018-06-21 19:26:17 +0200

    [➠] Updated TypoScript.

--- Configuration/TypoScript/Vendor/constants.ts
+++
@@ -1,5 +0,0 @@
-# === Includes ===
-
-<INCLUDE_TYPOSCRIPT: source="FILE: ./FluidStyledContent/constants.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/constants.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE: ./Replacecontent/constants.ts">
--- Configuration/TypoScript/Vendor/setup.ts
+++
@@ -1,5 +0,0 @@
-# === Includes ===
-
-<INCLUDE_TYPOSCRIPT: source="FILE: ./FluidStyledContent/setup.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/setup.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE: ./Replacecontent/setup.ts">
