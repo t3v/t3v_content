71654ddd1724aa4177c37875737371e29547d043
Maik Kempe <mkempe@bitaculous.com>
2020-09-15 16:41:05 +0200

    [➦] Merged changes.

---
+++ Tests/Unit/FirstClassTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace T3v\T3vContent\Tests\Unit;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use T3v\T3vContent\Tests\Unit\Fixtures\LoadableClass;
+
+/**
+ * The first class test class.
+ *
+ * @package T3v\T3vContent\Tests\Unit
+ */
+class FirstClassTest extends UnitTestCase
+{
+    /**
+     * Tests if the method returns true.
+     *
+     * @test
+     */
+    public function methodReturnsTrue(): void
+    {
+        $firstClassObject = new LoadableClass();
+
+        self::assertTrue($firstClassObject->returnsTrue());
+    }
+}
---
+++ Tests/Unit/Fixtures/LoadableClass.php
@@ -0,0 +1,15 @@
+<?php
+namespace T3v\T3vContent\Tests\Unit\Fixtures;
+
+/**
+ * The loadable class.
+ *
+ * @package T3v\T3vContent\Tests\Unit\Fixtures
+ */
+class LoadableClass
+{
+    public function returnsTrue(): bool
+    {
+        return true;
+    }
+}
