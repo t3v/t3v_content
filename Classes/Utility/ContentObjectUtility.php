5667c9d245fa9cfee305877c799472c56b3ef954
Maik Kempe <mkempe@bitaculous.com>
2022-04-27 15:39:04 +0200

    [✚] Added `ContentObject` utility.

---
+++ Classes/Utility/ContentObjectUtility.php
@@ -0,0 +1,65 @@
+<?php
+namespace T3v\T3vContent\Utility;
+
+/**
+ * The content object utility class.
+ *
+ * @package T3v\T3vContent\Utility
+ */
+class ContentObjectUtility {
+  /**
+   * Gets a content object identifier from an content object key.
+   *
+   * @param string $contentObjectKey The content object key
+   * @return string The content object identifier
+   */
+  public static function contentObjectIdentifier(string $contentObjectKey): string {
+    $contentObjectIdentifier = $contentObjectKey;
+
+    if (strpos($contentObjectIdentifier, '_') ||
+        strpos($contentObjectIdentifier, '-') ||
+        strpos($contentObjectIdentifier, ' ')) {
+      $contentObjectIdentifier = mb_strtolower($contentObjectKey);
+      $contentObjectIdentifier = str_replace(['_', '-'], ' ', $contentObjectIdentifier);
+      $contentObjectIdentifier = str_replace(' ', '', ucwords($contentObjectIdentifier));
+    }
+
+    if ($contentObjectIdentifier[0]) {
+      $contentObjectIdentifier[0] = mb_strtoUpper($contentObjectIdentifier[0]);
+    }
+
+    return $contentObjectIdentifier;
+  }
+
+  /**
+   * Alias for the `contentObjectIdentifier` function.
+   *
+   * @param string $contentObjectKey The content object key
+   * @return string The content object identifier
+   */
+  public static function getContentElementIdentifier(string $contentObjectKey): string {
+    return self::contentObjectIdentifier($contentObjectKey);
+  }
+
+  /**
+   * Gets a content object signature from an extension and content object identifier.
+   *
+   * @param string $extensionIdentifier The extension identifier
+   * @param string $contentObjectIdentifier The content object identifier
+   * @return string The content object signature
+   */
+  public static function contentObjectSignature(string $extensionIdentifier, string $contentObjectIdentifier): string {
+    return mb_strtolower($extensionIdentifier . '_' . $contentObjectIdentifier);
+  }
+
+  /**
+   * Alias for the `contentObjectSignature` function.
+   *
+   * @param string $extensionIdentifier The extension identifier
+   * @param string $contentObjectIdentifier The content object identifier
+   * @return string The content object signature
+   */
+  public static function getContentElementSignature(string $extensionIdentifier, string $contentObjectIdentifier): string {
+    return self::contentObjectSignature($extensionIdentifier, $contentObjectIdentifier);
+  }
+}
