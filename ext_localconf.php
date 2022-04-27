4ab06bf03dfd7e30c9531841538f76e37ac3d5d0
Maik Kempe <mkempe@bitaculous.com>
2022-04-27 14:52:34 +0200

    [➦] Merged changes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,11 @@ defined('TYPO3_MODE') or die();
 
 // === Variables ===
 
-$namespace           = 'T3v';
-$extensionKey        = $_EXTKEY;
-$extensionSignature  = \T3v\T3vCore\Utility\ExtensionUtility::extensionSignature($namespace, $extensionKey);
+$namespace = 'T3v';
+$extensionKey = $_EXTKEY;
+$extensionSignature = \T3v\T3vCore\Utility\ExtensionUtility::extensionSignature($namespace, $extensionKey);
 $configurationFolder = \T3v\T3vCore\Utility\ExtensionUtility::configurationFolder($extensionKey);
-$iconsFolder         = \T3v\T3vCore\Utility\ExtensionUtility::iconsFolder($extensionKey);
+$iconsFolder = \T3v\T3vCore\Utility\ExtensionUtility::iconsFolder($extensionKey);
 
 // === Page TSconfig ===
 
@@ -20,16 +20,16 @@ $iconsFolder         = \T3v\T3vCore\Utility\ExtensionUtility::iconsFolder($exten
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
   $extensionSignature,
 
-  // The unique name of the content element in upper camel case
+  // The unique name of the content element in upper camel case:
   'Spacer',
 
-  // The array holding the controller-action-combinations that are accessible
+  // The array holding the controller-action-combinations that are accessible:
   [
-    // The first controller and its first action will be the default
+    // The first controller and its first action will be the default:
     'ContentElements\SpacerContentElement' => 'index'
   ],
 
-  // The array of non-cachable controller-action-combinations (they must already be enabled)
+  // The array of non-cachable controller-action-combinations (they must already be enabled):
   [
     // 'ContentElements\SpacerContentElement' => 'index'
   ]
@@ -43,22 +43,22 @@ if (TYPO3_MODE === 'BE') {
   $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
   $icons = [
-    'Spacer Content Element'        => "{$iconsFolder}/ContentElements/SpacerContentElement.svg",
-    'Accordion Grid Element'        => "{$iconsFolder}/GridElements/AccordionGridElement.svg",
-    'Accord Grid Element'           => "{$iconsFolder}/GridElements/AccordGridElement.svg",
+    'Spacer Content Element' => "{$iconsFolder}/ContentElements/SpacerContentElement.svg",
+    'Accordion Grid Element' => "{$iconsFolder}/GridElements/AccordionGridElement.svg",
+    'Accord Grid Element' => "{$iconsFolder}/GridElements/AccordGridElement.svg",
     '2 Columns Layout Grid Element' => "{$iconsFolder}/GridElements/ColumnLayoutGridElement/2Columns.svg",
     '3 Columns Layout Grid Element' => "{$iconsFolder}/GridElements/ColumnLayoutGridElement/3Columns.svg",
-    'Conditionally Grid Element'    => "{$iconsFolder}/GridElements/ConditionallyGridElement.svg",
+    'Conditionally Grid Element' => "{$iconsFolder}/GridElements/ConditionallyGridElement.svg",
     'Document Section Grid Element' => "{$iconsFolder}/GridElements/DocumentSectionGridElement.svg",
-    'Panel Grid Element'            => "{$iconsFolder}/GridElements/PanelGridElement.svg",
-    'Timeline Grid Element'         => "{$iconsFolder}/GridElements/TimelineGridElement.svg",
-    'Event Grid Element'            => "{$iconsFolder}/GridElements/EventGridElement.svg",
-    'Viewport Grid Element'         => "{$iconsFolder}/GridElements/ViewportGridElement.svg"
+    'Panel Grid Element' => "{$iconsFolder}/GridElements/PanelGridElement.svg",
+    'Timeline Grid Element' => "{$iconsFolder}/GridElements/TimelineGridElement.svg",
+    'Event Grid Element' => "{$iconsFolder}/GridElements/EventGridElement.svg",
+    'Viewport Grid Element' => "{$iconsFolder}/GridElements/ViewportGridElement.svg"
   ];
 
   foreach ($icons as $name => $source) {
     $iconIdentifier = \T3v\T3vCore\Utility\IconUtility::iconIdentifier($name);
-    $iconSignature  = \T3v\T3vCore\Utility\IconUtility::iconSignature($extensionKey, $iconIdentifier);
+    $iconSignature = \T3v\T3vCore\Utility\IconUtility::iconSignature($extensionKey, $iconIdentifier);
 
     $iconRegistry->registerIcon(
       $iconSignature,
